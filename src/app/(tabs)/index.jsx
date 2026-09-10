d40a87f5a3c8c6be6e8c46cdeb7e634e149589d5
AR123456 <coloradoz99@gmail.com>
2026-09-10 06:49:13 -0400

    fix to index js

--- src/app/(tabs)/index.jsx
+++ src/app/(tabs)/index.jsx
@@ -4,14 +4,14 @@ import MacroGrid from "@/components/MacroGrid";
 import RecentMeals from "@/components/RecentMeals";
 import ReminderToggle from "@/components/ReminderToggle";
 import ShareButton from "@/components/ShareButton";
-import { getMeals, Meal } from "@/storage/meals";
+import { getMeals } from "@/storage/meals";
 import { globalStyles } from "@/styles/global";
 import { useFocusEffect } from "expo-router";
 import { useCallback, useState } from "react";
 import { Platform, ScrollView, Text, View } from "react-native";
 
 export default function HomeScreen() {
-  const [meals, setMeals] = useState<Meal[]>([]);
+  const [meals, setMeals] = useState([]);
 
   const loadMeals = async () => {
     const data = await getMeals();
