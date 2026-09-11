09c343effde0697dfefccc627aa20348777240cf
AR123456 <coloradoz99@gmail.com>
2026-09-11 15:29:13 -0400

    fixed copy button component

--- src/components/CopyButton.jsx
+++ src/components/CopyButton.jsx
@@ -2,15 +2,11 @@ import { Ionicons } from "@expo/vector-icons";
 // getting clipboard from expo
 import * as Clipboard from "expo-clipboard";
 import * as Haptics from "expo-haptics";
-import { Alert, StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import { Meal } from "@/storage/meals";
+import { Alert, StyleSheet, Text, TouchableOpacity } from "react-native";
+
 import { colors } from "@/styles/global";
 
-type CopyButtonProps = {
-  meals: Meal[];
-};
-
-const CopyButton = ({ meals }: CopyButtonProps) => {
+const CopyButton = ({ meals }) => {
   // handle copy
   const handleCopy = async () => {
     const totals = meals.reduce(
