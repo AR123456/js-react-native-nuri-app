a8a1302d173f018da3274b0041c677914cd0803f
AR123456 <coloradoz99@gmail.com>
2026-09-11 15:30:51 -0400

    fixing macro card

--- src/components/MacroGrid.jsx
+++ src/components/MacroGrid.jsx
@@ -1,13 +1,10 @@
 import { StyleSheet, View } from "react-native";
-import { Meal } from "@/storage/meals";
+
 import MacroCard from "./MacroCard";
 
-type MacroGridProps = {
-  meals: Meal[];
-};
 // confusing name this is a card like container for the meal all the macro nutrients shown in a card like component
 
-const MacroGrid = ({ meals }: MacroGridProps) => {
+const MacroGrid = ({ meals }) => {
   // add up macronutrients - array reduce method
   // array.reduce((accumulator, currentValue, currentIndex, array) => {
   // Return the updated accumulator
